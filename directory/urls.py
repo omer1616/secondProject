7ad3a35d5600db4d08134431590e351cb21dfe78
Omer <omerinci415@gmail.com>
2023-02-19 17:29:48 +0300

    added company update and detail

--- directory/urls.py
+++ directory/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.list_company),
     path('list-company/', views.list_company, name="list_company"),
+    path('list-company/<int:id>', views.company_detail, name="company_detail"),
+    path('list-company/update/<int:id>', views.company_update, name="company_update"),
     path('add-company/', views.add_company,  name="add_company"),
     path('list-person/', views.list_person, name="list_person"),
     path('add-person/', views.add_person,  name="add_person"),
--- directory/views.py
+++ directory/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect, reverse
-from django.http import HttpResponse,  JsonResponse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 from .models import *
 from .forms import CompanyForm
+
 
 # Create your views here.
 
@@ -21,15 +22,37 @@ def add_company(request):
         form.save()
     context = {
         "form": form,
-        "test":" fdsf"
+        "test": " fdsf"
     }
     return render(request, 'directory/add_company.html', context=context)
 
 
+def company_detail(request, id):
+    company = get_object_or_404(Company, id=id)
+    print(company)
+    context = {
+        "company":  company
+    }
+    return render(request, "directory/company_detail.html",context=context)
+
+
+def company_update(request, id):
+    company = get_object_or_404(Company, id=id)
+    form = CompanyForm(request.POST,  request.FILES or None, instance=company)
+    if form.is_valid():
+        form.save()
+        return redirect('list_company')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'directory/company_update.html', context=context)
 def delete_company(request, id):
     company = Company.objects.get(id=id)
     company.delete()
     return JsonResponse({"status": "True"})
+
 
 def list_person(request):
     company = Company.objects.all()
@@ -37,10 +60,8 @@ def list_person(request):
         'company': company
     }
 
-    return render(request, 'directory/list_person.html' )
+    return render(request, 'directory/list_person.html')
 
 
 def add_person(request):
-
-
-    return render(request, 'directory/add_person.html',)
+    return render(request, 'directory/add_person.html', )
